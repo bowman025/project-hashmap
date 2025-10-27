d2babba7abd661e6d65336f7395f6c06e7b71f9d
RstK <208619198+bowman025@users.noreply.github.com>
2025-10-27 13:18:58 +0100

    Create basic set up

---
+++ hashmap.js
@@ -0,0 +1,35 @@
+export class HashMap {
+    #loadFactor = 0.75;
+    #capacity = 16;
+    
+    hash(key) {
+
+    }
+    set(key, value) {
+
+    }
+    get(key) {
+
+    }
+    has(key) {
+
+    }
+    remove(key) {
+
+    }
+    length() {
+
+    }
+    clear() {
+
+    }
+    keys() {
+
+    }
+    values() {
+
+    }
+    entries() {
+        
+    }
+}
